6cc61aedb8b218df2aab061d5fde14d9f620d1d4
sarahewson <sphsara03@gmail.com>
2023-11-24 20:58:21 +1300

    commit

--- dbConfig.js
+++ dbConfig.js
@@ -1,39 +1,16 @@
-// !!Currently import MySQL codes are in server.js file. Not linked to dbConfig.js file like WaterSafety app.
+const mysql = require('mysql');
+// const dotenv = require('dotenv');
+// create database connection.
+const db = mysql.createConnection({ 
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'stills'
+}); 
 
+db.connect(function(err) {
+	if (err) throw err;
+	console.log('MySQL database is connected.');
+});
 
-// Edited based on the Youtube tutorial
-// https://www.youtube.com/watch?v=Ch05xM5ot1o&list=PLD9SRxG6ST3GBsczn8OUKLaErhrvOz9zQ&index=2
-
-// const mysql = require('mysql'); 	// import mySQL.
-// const db = mysql.createConnection({
-// 	host: 'localhost', // Replace with your host name
-// 	user: 'root',      // Replace with your database username
-// 	password: '',      // Replace with your database password
-// 	database: 'stills' // // Replace with your database Name
-// }); 
-
-// db.connect(function(err) {
-// 	if (err) throw err;
-// 	console.log('MySQL database is connected successfully!');
-// });
-
-// module.exports = db;
-
-// -------------------------------------------------
-
-// WaterSafety dbConfig.js code
-
-// var mysql = require('mysql');
-// var conn = mysql.createConnection({
-// 	host: 'localhost', // Replace with your host name
-// 	user: 'root',      // Replace with your database username
-// 	password: '',      // Replace with your database password
-// 	database: 'db2' // // Replace with your database Name
-// }); 
-
-// conn.connect(function(err) {
-// 	if (err) throw err;
-// 	console.log('Database is connected successfully !');
-// });
-
-// module.exports = conn;
+module.exports = db;
